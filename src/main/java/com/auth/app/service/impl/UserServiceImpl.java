2f5c3d8cff58623f68373af6a4230c992b4c0a7a
mihir-parikh-11 <mihirparikh113@gmail.com>
2025-08-23 01:41:52 +0530

    feat: Add Logger Logic

--- src/main/java/com/auth/app/service/impl/UserServiceImpl.java
+++ src/main/java/com/auth/app/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import com.auth.app.service.dto.UserDetailsDTO;
 import com.auth.app.service.mapper.UserMapper;
 import com.auth.app.utility.UserUtility;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
@@ -29,25 +31,33 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean existsByEmail(String email) {
+        log.info("Request for check email : {} exists", email);
         return userRepository.existsByEmail(email);
     }
 
     @Override
     public boolean existsByUsername(String username) {
+        log.info("Request for check username : {} exists", username);
         return userRepository.existsByUsername(username);
     }
 
     @Override
     public User findByUsername(String username) {
+        log.info("Request for get User by Username : {}", username);
         return userRepository.findByUsername(username).orElseThrow(() -> new GlobalException(ConstantMessage.USER_NOT_FOUND_MESSAGE, ConstantApplicationCode.USER_NOT_FOUND_APPLICATION_CODE, HttpStatus.BAD_REQUEST));
     }
 
     @Override
     public User registerUser(RegisterUserDTO registerUserDTO) {
-        if (existsByEmail(registerUserDTO.getEmail()))
+        log.info("Request for register user : {} ", registerUserDTO);
+        if (existsByEmail(registerUserDTO.getEmail())) {
+            log.warn("Email {} Already exist", registerUserDTO.getEmail());
             throw new GlobalException(ConstantMessage.EMAIL_ALREADY_EXIST_MESSAGE, ConstantApplicationCode.EMAIL_ALREADY_EXIST_APPLICATION_CODE, HttpStatus.BAD_REQUEST);
-        if (existsByPhoneNumber(registerUserDTO.getPhoneNumber()))
+        }
+        if (existsByPhoneNumber(registerUserDTO.getPhoneNumber())) {
+            log.warn("Phone number {} Already exist", registerUserDTO.getPhoneNumber());
             throw new GlobalException(ConstantMessage.PHONE_NUMBER_ALREADY_EXIST_MESSAGE, ConstantApplicationCode.PHONE_NUMBER_ALREADY_EXIST_APPLICATION_CODE, HttpStatus.BAD_REQUEST);
+        }
         User user = userMapper.toEntity(registerUserDTO);
         user.setPassword(passwordEncoder.encode(registerUserDTO.getPassword()));
         user.setStatus(true);
@@ -56,16 +66,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean existsByPhoneNumber(Long phoneNumber) {
+        log.info("Request for check Phone number : {} exists", phoneNumber);
         return userRepository.existsByPhoneNumber(phoneNumber);
     }
 
     @Override
     public UserDetailsDTO getCurrentLoginUser() {
+        log.info("Request for get Current login user");
         return userMapper.toDto(UserUtility.getCurrentLoginUser());
     }
 
     @Override
     public List<UserDetailsDTO> getAllUser() {
+        log.info("Request for get all user");
         return userRepository.findAllByUsernameIsNot(UserUtility.getCurrentLoginUsername())
                 .stream()
                 .map(userMapper::toDto)
